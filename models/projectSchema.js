c2f056e53e74916e20a24d21f2b5550a8500b947
Awsmchrlz <chisalecharles23@gmail.com>
2024-12-24 13:36:11 +0200

    post routes

---
+++ models/projectSchema.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+// Clear the existing model if it exists
+if (mongoose.connection.models['client']) {
+  delete mongoose.connection.models['client']
+}
+
+const ProjectSchema = new mongoose.Schema({
+ 
+  phoneNumber: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  city: {
+    type: String,
+    required: true
+  },
+  country: {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    required: true,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model("Project", ProjectSchema);
