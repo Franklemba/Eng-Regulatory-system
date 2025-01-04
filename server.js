5ddc85bbee078e77aae84df72693ee4fc925102b
Awsmchrlz <chisalecharles23@gmail.com>
2025-01-04 16:25:09 +0200

    starts

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ const localDB = "mongodb://127.0.0.1:27017/EngRegulatoryBoard"
 const liveDB = "mongodb+srv://Engineering:96EceAsGquKn3aLt@cluster0.cq29s.mongodb.net/?retryWrites=true&w=majority&appName=Cluster096EceAsGquKn3aLt"
 mongoose.set('strictQuery', true);
 
-const mainDB = liveDB
+const mainDB = localDB
 
 
 mongoose.connect(mainDB,{useNewUrlParser: true}).then(() => {
